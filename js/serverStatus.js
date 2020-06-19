50213f19f99bfd5db387aa451de4da7fa94d55ea
Pranav Krishna <krishnatechpranav@gmail.com>
2020-06-19 15:12:45 -0700

    12

--- js/serverStatus.js
+++ js/serverStatus.js
@@ -33,27 +33,27 @@ function getServerManagementInfo() {
             var serverLastUptime = data['serverWentUp'];
 
             var serverLastDowntime = data['lastDownTime'];
-        //Get Server Uptime 
+            //Get Server Uptime 
 
-        var startDate = new Date(serverLastDowntime);
-        // Do your operations
-        var endDate   = new Date();
-        var seconds = (endDate.getTime() - startDate.getTime()) / 1000;
-                   
-                    given_seconds = seconds; 
-          
-                    dateObj = new Date(given_seconds * 1000); 
-                    hours = dateObj.getUTCHours(); 
-                    minutes = dateObj.getUTCMinutes(); 
-                    seconds = dateObj.getSeconds(); 
-          
-                    timeString = hours.toString().padStart(2, '0') 
-                        + ':' + minutes.toString().padStart(2, '0') 
-                        + ':' + seconds.toString().padStart(2, '0'); 
-          
-        console.log(timeString);
+            var startDate = new Date(serverLastDowntime);
+            // Do your operations
+            var endDate = new Date();
+            var seconds = (endDate.getTime() - startDate.getTime()) / 1000;
 
-        $('#serverUptime').html(timeString);
+            given_seconds = seconds;
+
+            dateObj = new Date(given_seconds * 1000);
+            hours = dateObj.getUTCHours();
+            minutes = dateObj.getUTCMinutes();
+            seconds = dateObj.getSeconds();
+
+            timeString = hours.toString().padStart(2, '0')
+                + ':' + minutes.toString().padStart(2, '0')
+                + ':' + seconds.toString().padStart(2, '0');
+
+            console.log(timeString);
+
+            $('#serverUptime').html(timeString);
 
         } else if (serverStatus == false) {
 
@@ -77,7 +77,7 @@ function getServerManagementInfo() {
             $('#lastServerDownTime').html(serverLastDowntime);
         }
 
-        
+
 
 
 
@@ -94,19 +94,19 @@ function changeServerStatus() {
         if (checkedStatus == true) {
 
             var today = new Date();
-            var todayFormatted = today.getFullYear() + "/" + (today.getMonth() + 1) + "/" + today.getDate() + " " +  today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+            var todayFormatted = today.getFullYear() + "/" + (today.getMonth() + 1) + "/" + today.getDate() + " " + today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
 
             firebase.firestore().collection("Application Management").doc("ServerManagement").update({
                 "serversAreUp": true,
                 "serverWentUp": todayFormatted,
-                
+
             });
 
 
         } else {
 
             var today = new Date();
-            var todayFormatted = today.getFullYear() + "/" + (today.getMonth() + 1) + "/" + today.getDate() + " " +  today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+            var todayFormatted = today.getFullYear() + "/" + (today.getMonth() + 1) + "/" + today.getDate() + " " + today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
 
             firebase.firestore().collection("Application Management").doc("ServerManagement").update({
                 "serversAreUp": false,
@@ -133,4 +133,70 @@ function changeServerStatus() {
 
 
     console.log(checkedStatus);
+}
+
+function sendServerAlert() {
+    var title = document.getElementById('alertTitle').value;
+    var message = document.getElementById('alertDescription').value;
+    var alertHours = document.getElementById('alertHours').value;
+    var alertMinutes = document.getElementById('alertMinutes').value;
+    var alertSeconds = document.getElementById('alertSeconds').value;
+    var serverManagerKey = document.getElementById('serverManagerKeyAlertInput').value;
+
+
+    if (title, message, alertHours, alertMinutes, alertSeconds, serverManagerKey == '') {
+        var alertHTML = `
+        <div class="alert alert-danger" role="alert">
+        You cannot leave any fields blank
+      </div>
+        `;
+        $('#alertSendError').html(alertHTML);
+    } else {
+
+        if (serverManagerKey == 'tdoSeexYY/sM/VQ==994jmc903t48hs3e3dsf4rf') {
+
+            $('#alertSendError').html("");
+
+            firebase.firestore().collection('ApplicationManagement').doc("ServerAlerts").set({
+                alertTitle: title,
+                alertMessage: message, 
+            });
+
+            document.getElementById('waitSection').style.display = "initial";
+            document.getElementById('alertInput').style.display = "none";
+
+            var time = Number(alertSeconds) + (Number(alertMinutes) * 60) + (Number(alertHours) * 60 * 60),
+
+            display = document.querySelector('#time');
+            startTimer(time, display);
+
+        } else {
+            var alertHTML = `
+            <div class="alert alert-danger" role="alert">
+            The Server Manager Key is incorrect.
+          </div>
+            `;
+            $('#alertSendError').html(alertHTML);
+        }
+    }
+
+}
+
+function startTimer(duration, display) {
+    var timer = duration, minutes, seconds;
+
+    setInterval(function () {
+        minutes = parseInt(timer / 60, 10);
+        seconds = parseInt(timer % 60, 10);
+
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        console.log(minutes + ":" + seconds);
+
+        if (--timer < 0) {
+            timer = duration;
+            console.log("timer done");
+        }
+    }, 1000);
 }
